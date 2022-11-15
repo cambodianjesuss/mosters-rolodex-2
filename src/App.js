083b1a0288cd8790c91ff526a525e6a0dc411a68
Boi Soth <cambodianjesuss@gmail.com>
2022-11-14 23:58:33 -0800

    fetching data then setting state

--- src/App.js
+++ src/App.js
@@ -16,7 +16,14 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => console.log(users));
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state),
+        ),
+      );
   }
 
   render() {
