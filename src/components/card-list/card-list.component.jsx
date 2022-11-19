08b8be36f8fe05bb6d97c7ccff7315d241e7b8e8
Boi Soth <cambodianjesuss@gmail.com>
2022-11-19 02:05:14 -0800

    adding style to card container

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
 
+import './card-list.styles.css'
+
 class CardList extends Component {
   render(){
 
     const { monsters }= this.props
     return (
-      <div>
+      <div className="card-list">
       { monsters.map(monster =>{
         const {id, name, email} = monster;
         return(
