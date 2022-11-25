49c6ec7d095bb50f0e9f20c60de0327c0a91ed85
Boi Soth <cambodianjesuss@gmail.com>
2022-11-25 01:32:07 -0800

    using effect for search field change to no rerender

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component";
 
 import "./App.css";
+import userEvent from "@testing-library/user-event";
 
 const App = () => {
   const [searchField, setSearchField] = useState(""); // [value, setValue]
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   // First argument is a callback function we want to use an effect on our functional component
   // Second argument is and array of our dependencies in state values*/prop values in context of application
@@ -19,14 +21,18 @@ const App = () => {
       .then((users) => setMonsters(users));
   }, []);
 
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
-
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLocaleLowerCase().includes(searchField);
-  });
 
   return (
     <div className="App">
